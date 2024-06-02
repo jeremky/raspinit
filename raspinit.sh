f4f58838a62320f5c72aa277ee321d3639a7a2ea
JeremKy <jeremky@me.com>
2024-06-02 18:10:32 +0200

    first commit

---
+++ raspinit.sh
@@ -0,0 +1,66 @@
+#!/bin/dash
+set -e
+
+## Lecture de la config
+dir=$(dirname "$0")
+cfg="$dir/$(basename -s .sh $0).cfg"
+
+## Vérification
+if [ ! -f /usr/bin/raspi-config ] ; then
+    echo "Incompatible !"
+    exit 0
+fi
+
+## Config
+if [ -f $cfg ] ; then
+    . $cfg
+else
+    echo "Fichier $cfg introuvable"
+    exit 0
+fi
+
+## User
+if [ "$USER" != "root" ] ; then
+    echo "Droits root nécessaires"
+    exit 0
+fi
+
+## Alias
+echo "" >> /etc/profile
+echo "## Temperature" >> /etc/profile
+echo "alias temp='sudo vcgencmd measure_temp'" >> /etc/profile
+
+## Swap
+if [ $swap = "off" ] ; then
+    swapoff --all
+    apt -y remove dphys-swapfile
+    apt -y autoremove
+    rm -f /var/swap
+fi
+
+## Wifi
+if [ $wifi = "off" ] ; then
+    echo "dtoverlay=disable-wifi" | tee -a /boot/firmware/config.txt
+    systemctl disable wpa_supplicant
+    echo "Wifi désactivé"
+fi
+
+## Bluetooth
+if [ $bluetooth = "off" ] ; then
+    echo "dtoverlay=disable-bt" | tee -a /boot/firmware/config.txt
+    systemctl disable hciuart
+    echo "Bluetooth désactivé"
+fi
+
+## Ip config
+echo "" >> /etc/dhcpcd.conf
+echo "# Static IP configuration" | tee -a /etc/dhcpcd.conf
+echo "interface eth0" | tee -a /etc/dhcpcd.conf
+echo "static ip_address=$ip/24" | tee -a /etc/dhcpcd.conf
+echo "static routers=$gateway" | tee -a /etc/dhcpcd.conf
+echo "static domain_name_servers=$dns" | tee -a /etc/dhcpcd.conf
+
+## Log2ram
+if [ -f $dir/log2ram/log2ram.sh ] ; then
+    $dir/log2ram/log2ram.sh
+fi
