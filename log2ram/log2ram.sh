7f96fbf0a08707e36d34a41637cd50948bda4db3
JeremKy <jeremky@me.com>
2024-12-15 21:14:59 +0100

    Passage des tabulations à 2

--- log2ram/log2ram.sh
+++ log2ram/log2ram.sh
@@ -5,28 +5,28 @@ dir=$(dirname "$0")
 
 ## User
 if [ "$USER" != "root" ] ; then
-    echo "Droits root nécessaires"
-    exit 0
+  echo "Droits root nécessaires"
+  exit 0
 fi
 
 ## Installation du dépot
 if [ ! -f /usr/local/bin/log2ram ] ; then
-    echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bookworm main" | tee /etc/apt/sources.list.d/azlux.list
-    wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
-    apt update
-    apt -y install rsync log2ram
-    cp $dir/log2ram.cfg /etc/log2ram.conf
-    ## Redemarrage
-    read -p "Redémarrage nécessaire. Confirmer (o/n): " reponse
-    case $reponse in
-        o)
-            reboot
-            ;;
-        *)
-            echo "Redemarrez avant toute autre installation !"
-            exit 0
-            ;;
-    esac
+  echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bookworm main" | tee /etc/apt/sources.list.d/azlux.list
+  wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
+  apt update
+  apt -y install rsync log2ram
+  cp $dir/log2ram.cfg /etc/log2ram.conf
+  ## Redemarrage
+  read -p "Redémarrage nécessaire. Confirmer (o/n): " reponse
+  case $reponse in
+    o)
+      reboot
+      ;;
+    *)
+      echo "Redemarrez avant toute autre installation !"
+      exit 0
+      ;;
+  esac
 else
-    echo "log2ram est déjà installé"
+  echo "log2ram est déjà installé"
 fi
--- raspinit.sh
+++ raspinit.sh
@@ -7,22 +7,22 @@ cfg="$dir/$(basename -s .sh $0).cfg"
 
 ## Vérification
 if [ ! -f /usr/bin/raspi-config ] ; then
-    echo "Incompatible !"
-    exit 0
+  echo "Incompatible !"
+  exit 0
 fi
 
 ## Config
 if [ -f $cfg ] ; then
-    . $cfg
+  . $cfg
 else
-    echo "Fichier $cfg introuvable"
-    exit 0
+  echo "Fichier $cfg introuvable"
+  exit 0
 fi
 
 ## User
 if [ "$USER" != "root" ] ; then
-    echo "Droits root nécessaires"
-    exit 0
+  echo "Droits root nécessaires"
+  exit 0
 fi
 
 ## Alias
@@ -32,27 +32,27 @@ echo "alias temp='sudo vcgencmd measure_temp'" >> /etc/profile
 
 ## Swap
 if [ $swap = "off" ] ; then
-    swapoff --all
-    apt -y remove dphys-swapfile
-    apt -y autoremove
-    rm -f /var/swap
+  swapoff --all
+  apt -y remove dphys-swapfile
+  apt -y autoremove
+  rm -f /var/swap
 fi
 
 ## Wifi
 if [ $wifi = "off" ] ; then
-    echo "dtoverlay=disable-wifi" | tee -a /boot/firmware/config.txt
-    systemctl disable wpa_supplicant
-    echo "Wifi désactivé"
+  echo "dtoverlay=disable-wifi" | tee -a /boot/firmware/config.txt
+  systemctl disable wpa_supplicant
+  echo "Wifi désactivé"
 fi
 
 ## Bluetooth
 if [ $bluetooth = "off" ] ; then
-    echo "dtoverlay=disable-bt" | tee -a /boot/firmware/config.txt
-    systemctl disable hciuart
-    echo "Bluetooth désactivé"
+  echo "dtoverlay=disable-bt" | tee -a /boot/firmware/config.txt
+  systemctl disable hciuart
+  echo "Bluetooth désactivé"
 fi
 
 ## Log2ram
 if [ -f $dir/log2ram/log2ram.sh ] ; then
-    $dir/log2ram/log2ram.sh
+  $dir/log2ram/log2ram.sh
 fi
