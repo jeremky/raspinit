38d60db58cc4ae5561039e9102ff0840577ec9cf
JeremKy <jeremky@me.com>
2025-07-21 11:11:49 +0200

    Mise à jour

--- raspinit.sh
+++ raspinit.sh
@@ -2,7 +2,7 @@
 
 dir=$(dirname "$(realpath "$0")")
 
-# Messages colorisés
+# Messages en couleur
 error()    { echo -e "\033[0;31m====> $*\033[0m" ;}
 message()  { echo -e "\033[0;32m====> $*\033[0m" ;}
 warning()  { echo ; echo -e "\033[0;33m====> $*\033[0m" ;}
